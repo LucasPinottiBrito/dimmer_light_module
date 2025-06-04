b000c58d529a94ecb64ce80d22e9f400c2c4f936
LucasPinottiBrito <lucaspinottibrito@gmail.com>
2025-06-04 11:35:44 -0300

    crazy mode and raise mode

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,38 @@ MqttClientHelper mqttClient(
     "Rfid_device1"
 );
 
+// Modo que varia brilho aleatoriamente por 15s
+void crazyMode() {
+    Serial.println("🔀 Crazy Mode ON");
+    unsigned long start = millis();
+
+    while (millis() - start < 15000) {
+        int pct = random(0, 101);
+        dimmerSetIntensity(pct);
+        Serial.printf("Crazy brightness: %d%%\n", pct);
+        delay(1000);
+    }
+
+    Serial.println("🔀 Crazy Mode OFF");
+}
+
+// Modo que sobe de 0 a 100% em 10s
+void raiseMode() {
+    Serial.println("⏫ Raise Mode ON");
+
+    const int duration = 5000;
+    const int steps = 90;
+    const int delayPerStep = duration / steps;
+
+    for (int pct = 10; pct <= 100; pct++) {
+        dimmerSetIntensity(pct);
+        Serial.printf("Raise brightness: %d%%\n", pct);
+        delay(delayPerStep);
+    }
+
+    Serial.println("⏫ Raise Mode OFF");
+}
+
 void setup() {
     Serial.begin(115200);
     WiFiManagerHelper::connect();
@@ -28,7 +60,17 @@ void setup() {
         if (strcmp(topic, "/smartroom/dimmer/control") == 0) {
             char buffer[6] = {0};
             strncpy(buffer, (char*)payload, min(length, 5U));
+
             int pct = atoi(buffer);
+
+            if (pct == 43) {
+                crazyMode();
+                return;
+            } else if (pct == 17) {
+                raiseMode();
+                return;
+            }
+
             pct = constrain(pct, 0, 100);
             dimmerSetIntensity(pct);
             Serial.printf("Brilho ajustado: %d%%\n", pct);
